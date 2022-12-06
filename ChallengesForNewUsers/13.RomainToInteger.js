5d7a829157fbd070fca3cd3e8eac4f0557fe3b3b
Nancy Do <nancydo272@gmail.com>
2022-12-05 21:08:39 -0500

    first problem

---
+++ ChallengesForNewUsers/13.RomainToInteger.js
@@ -0,0 +1,17 @@
+//Given a roman numeral, convert it to an integer
+//Ex1: s="III" output=3
+
+var romanToInt = function(s) {
+    var I = 1; 
+    var V = 5; 
+    var X = 10; 
+    var L = 50; 
+    var C = 100; 
+    var D = 500; 
+    var M = 1000; 
+    for(var i = 0; i<s.length; i++){
+        
+    }
+};
+
+romanToInt("III"); 
